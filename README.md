5d0dc1cf27f0c5ee71ea45191197be157581b937
sri <ssrikanthpusarla@gmail.com>
2020-06-17 16:45:47 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/1315e116650345da9177b96c327f7901)](https://app.codacy.com/manual/srivatsams/tailspin-spacegame-web?utm_source=github.com&utm_medium=referral&utm_content=srivatsamarichi/tailspin-spacegame-web&utm_campaign=Badge_Grade_Dashboard)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=playgroundgithub&metric=alert_status)](https://sonarcloud.io/dashboard?id=playgroundgithub)
+![Spacegame.Web.Docker](https://github.com/srivatsamarichi/tailspin-spacegame-web/workflows/Spacegame.Web.Docker/badge.svg)
+![Spacegame.Web](https://github.com/srivatsamarichi/tailspin-spacegame-web/workflows/Spacegame.Web/badge.svg)
+![Spacegame.Web.Docker.GHPackages](https://github.com/srivatsamarichi/tailspin-spacegame-web/workflows/Spacegame.Web.Docker.GHPackages/badge.svg)
+
+## Overview
+
+Tailspin Toys, or Tailspin for short, is a video game company. Tailspin hosts its game servers and websites in an on-premises data-center. The company just celebrated the release of a new racing game. They'll be releasing a space shooter game, called Space Game, in the coming months.
+
+The team builds websites to support new game titles. These websites provide information about the game, ways to get it, and leader-boards that show top scores. Each website must go live the same day the game is released.
+
+The Space Game website is a .NET Core app written in C# that's deployed to Linux. The website isn't finished yet, but here's what it looks like right now:
+
+![](https://docs.microsoft.com/en-us/learn/azure-devops/assess-your-development-process/media/2-space-game-top.png)
+
+# Codeacy is awesome
---
+++ Tailspin.SpaceGame.Web/Startup.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TailSpin.SpaceGame.Web.Models;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+
+
+namespace TailSpin.SpaceGame.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            // Add document stores. These are passed to the HomeController constructor.
+            services.AddSingleton<IDocumentDBRepository<Score>>(new LocalDocumentDBRepository<Score>(@"SampleData/scores.json"));
+            services.AddSingleton<IDocumentDBRepository<Profile>>(new LocalDocumentDBRepository<Profile>(@"SampleData/profiles.json"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            var unused = app.UseEndpoints(endpoints =>
+                {
+
+                    app.UseRouting();
+
+#pragma warning disable ASP0001 // Authorization middleware is incorrectly configured.
+                    app.UseAuthorization();
+#pragma warning restore ASP0001 // Authorization middleware is incorrectly configured.
+
+                    app.UseEndpoints(endpoints =>
+                    {
+                        endpoints.MapControllerRoute(
+                            name: "default",
+                            pattern: "{controller=Home}/{action=Index}/{id?}");
+                    });
+                });
+        }
+    }
+}
